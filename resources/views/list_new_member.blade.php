490d9eb14d8bc62f67c4bc288b88334e24111c7a
Fatahillah_Abid <abidkudus@gmail.com>
2025-01-03 11:09:12 +0700

    remove unused PHP code for DataTables configuration in list_new_member view

--- resources/views/list_new_member.blade.php
+++ resources/views/list_new_member.blade.php
@@ -9,40 +9,6 @@
 {{-- Content body: main page content --}}
 
 @section('content_body')
-    {{-- PHP for Datatables Tool --}}
-    {{-- @php
-        $heads = [
-            'ID',
-            'Name',
-            ['label' => 'Phone', 'width' => 40],
-            ['label' => 'Actions', 'no-export' => true, 'width' => 5],
-        ];
-
-        $btnEdit = '<button class="btn btn-xs btn-default text-primary mx-1 shadow" title="Edit">
-                <i class="fa fa-lg fa-fw fa-pen"></i>
-            </button>';
-        $btnDelete = '<button class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
-                  <i class="fa fa-lg fa-fw fa-trash"></i>
-              </button>';
-        $btnDetails = '<button class="btn btn-xs btn-default text-teal mx-1 shadow" title="Details">
-                   <i class="fa fa-lg fa-fw fa-eye"></i>
-               </button>';
-
-        $config = [
-            'data' => [
-                [22, 'John Bender', '+02 (123) 123456789', '<nobr>' . $btnEdit . $btnDelete . $btnDetails . '</nobr>'],
-                [
-                    19,
-                    'Sophia Clemens',
-                    '+99 (987) 987654321',
-                    '<nobr>' . $btnEdit . $btnDelete . $btnDetails . '</nobr>',
-                ],
-                [3, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>' . $btnEdit . $btnDelete . $btnDetails . '</nobr>'],
-            ],
-            'order' => [[1, 'asc']],
-            'columns' => [null, null, null, ['orderable' => false]],
-        ];
-    @endphp --}}
     {{-- Table --}}
     <table id="users-table" class="table table-bordered">
         <thead>
@@ -59,7 +25,6 @@
     </table>
 
     {{-- Modal Edit --}}
-    <!-- Modal Edit -->
     <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
